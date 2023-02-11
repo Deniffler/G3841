e99e736557da67bc8fba0d45585c932b18bfd682
Денис Ковалев <deniffler@MacBook-Pro-Denis.local>
2023-02-11 18:49:35 +0500

    Add solution for task 64

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 64: 
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+
+
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(string prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+// Функция рекурсии
+
+string LineGenRec(int num)
+{
+    if (num == 0)
+    {
+        return string.Empty;
+    }
+    else
+    {
+        //return LineGenRec(num - 1) + " " + num ;
+        return num + " " + LineGenRec (num - 1);
+    }
+}
+
+int number = ReadData("Введите число N: ");
+string resultLine = LineGenRec(number);
+PrintResult(resultLine);
