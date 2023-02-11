6408ade61b78306df222e7fcf48ad5092adb1104
Денис Ковалев <deniffler@MacBook-Pro-Denis.local>
2023-02-11 19:10:53 +0500

    Add solution for task 66

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 66: 
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+
+// Метод рекурсии вычисляет сумму чисел от M до N
+ int GetSum(int m, int n)
+{
+    if (n > m)
+        return n + GetSum(m, n - 1);
+    return m;
+}
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод выводит результат пользователю
+void PrintData(string msg, int res)
+{
+    Console.WriteLine(msg + res);
+}
+
+// Основной код программы
+// 1  Вводим данные
+int m = ReadData("Введите число M: ");
+int n = ReadData("Введите число N: ");
+
+// 2 Считаем сумму
+int sum = GetSum(m,n);
+
+// Выводим результат
+PrintData("Сумма элементов от М до N равна: ",sum);
+
+
+
